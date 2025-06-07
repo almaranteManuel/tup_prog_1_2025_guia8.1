ffdcd09808ace551d99cc2be00e20d2ef79b206d
= <=>
2025-06-07 14:20:22 -0300

    Ejercicio 1 terminado

---
+++ Guia 8.1/Ejercicio 1/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        //Instanciamos la clase Servicio
+        static Servicio servicio = new Servicio();
+
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.WriteLine(@"Seleccione una opción del menú:
+                1. Procesar un solo número
+                2. Procesar varios números
+                3. Mostrar máximo y mínimo.
+                4. Mostrar promedio.
+                5. Mostrar cantidad de números ingresados.
+                6. Reiniciar variables
+                0. Salir.
+            ");
+
+            int op = Convert.ToInt32(Console.ReadLine());
+
+            return op;
+        }
+
+        static void MostrarIniciarVariables()
+        {
+            servicio = new Servicio();
+            Console.WriteLine("Variables reiniciadas.");
+            Console.ReadKey();
+        }
+
+        static void MostrarPantallaSolicitarNumero()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese un número:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            servicio.RegistrarValor(numero);
+            Console.WriteLine("Número registrado.");
+            Console.ReadKey();
+        }
+
+        static void MostrarPantallaSolicitarVariosNumeros()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Ingrese la cantidad de números a procesar");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine($"Ingrese el número {i + 1}:");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                servicio.RegistrarValor(numero);
+            }
+            Console.ReadKey();
+        }
+
+        static void MostrarPantallaMaximoMinimo()
+        {
+            Console.Clear();
+            Console.WriteLine($"Máximo: {servicio.Maximo}");
+            Console.WriteLine($"Mínimo: {servicio.Minimo}");
+            Console.ReadKey();
+        }
+
+        static void MostrarPantallaCalcularYMostrarPromedio()
+        {
+            Console.Clear();
+            double promedio = servicio.CalcularPromedio();
+            Console.WriteLine($"El promedio es: {promedio}");
+            Console.ReadKey();
+        }
+
+        static void MostrarPantallaCantidad()
+        {
+            Console.Clear();
+            Console.WriteLine($"Cantidad de números ingresados: {servicio.CantidadNumerosIngresados}");
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+
+            //MostrarIniciarVariables();
+
+            int op = MostrarPantallaSolicitarOpcionMenu();
+
+            while (op != 0)
+            {
+                switch (op)
+                {
+                    case 1:
+                        MostrarPantallaSolicitarNumero();
+                        break;
+                    case 2:
+                        MostrarPantallaSolicitarVariosNumeros();
+                        break;
+                    case 3:
+                        MostrarPantallaMaximoMinimo();
+                        break;
+                    case 4:
+                        MostrarPantallaCalcularYMostrarPromedio();
+                        break;
+                    case 5:
+                        MostrarPantallaCantidad();
+                        break;
+                    case 6:
+                        MostrarIniciarVariables();
+                        break;
+                    default:
+                        op = 0;
+                        break;
+                }
+
+                op = MostrarPantallaSolicitarOpcionMenu();
+
+            }
+        }
+    }
+}
---
+++ Guia 8.1/Ejercicio 1/Servicio.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    class Servicio
+    {
+        int acumulador = 0;
+        int contador = 0;
+        int maximo = 0;
+        int minimo = 0;
+
+        public int Maximo
+        {
+            get { return maximo; }
+        }
+        public int Minimo
+        {
+            get { return minimo; }
+        }
+        public int CantidadNumerosIngresados
+        {
+            get { return contador; }
+        }
+
+        public Servicio()
+        {
+        }
+
+        public void RegistrarValor(int numero)
+        {
+            if (contador == 0)
+            {
+                minimo = numero;
+                maximo = numero;
+            }
+            else
+            {
+                if (numero < minimo)
+                {
+                    minimo = numero;
+                }
+                if (numero > maximo)
+                {
+                    maximo = numero;
+                }
+            }
+            acumulador += numero;
+            contador++;
+        }
+
+        public double CalcularPromedio()
+        {
+            if (contador == 0)
+            {
+                return 0;
+            }
+            return 1.0*acumulador / contador;
+        }
+    }
+}
